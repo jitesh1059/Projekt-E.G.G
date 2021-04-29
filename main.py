7262b79bbad3dc9923417b1589eb7922035d6e74
Jitesh <jiteshmoganaraja@gmail.com>
2021-04-29 13:33:19 +0800

    Player Movement

--- main.py
+++ main.py
@@ -13,16 +13,68 @@ WIDTH, HEIGHT = 1280, 720
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Projekt E.G.G")
 
+# Player
+PLAYER_IMG = pygame.image.load(os.path.join("assets", "egg.png"))
+
+# Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background.jpg")), (WIDTH, HEIGHT))
+
+class Ship:
+    COOLDOWN = 30
+
+    def __init__(self, x, y, health=1000):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def get_width(self):
+        return self.ship_img.get_width()
+
+    def get_height(self):
+        return self.ship_img.get_height()
+
+
+class Player(Ship):
+    def __init__(self, x, y, health=1000):
+        super().__init__(x, y, health)
+        self.ship_img = PLAYER_IMG
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
+
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))
+        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health/self.max_health), 10))
 
 def main():
     run = True
     FPS = 60
+    player_vel = 5
+    player = Player(300, 630)
 
     clock = pygame.time.Clock()
 
     def redraw_window():
         WIN.blit(BG, (0,0))
+        
+        player.draw(WIN)
+
         pygame.display.update()
 
     while run:
@@ -34,16 +86,10 @@ def main():
                 quit()
 
         keys = pygame.key.get_pressed()
-        vel_x = 0
-        vel_y = 0
-        if keys[pygame.K_a]: # left
-            print(vel_x - 5)
-        if keys[pygame.K_d]: # right
-            print(vel_x + 5)
-        if keys[pygame.K_w]: # up
-            print(vel_y + 5)
-        if keys[pygame.K_s]: # down
-            print(vel_y - 5)
+        if keys[pygame.K_a] and player.x - player_vel > 0: # left
+            player.x -= player_vel
+        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH: # right
+            player.x += player_vel
         if keys[pygame.K_ESCAPE]:
             run = False
 
